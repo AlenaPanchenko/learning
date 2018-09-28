046a3498631e73a13c4cd242fba1ec038b5c23e1
Alena Panchenko <alena.panchenko22@gmail.com>
2018-09-28 14:26:28 +0300

    add leap years exs

---
+++ leap_years.rb
@@ -0,0 +1,8 @@
+# leap_years excercise
+puts 'Hello! What is a starting year?'
+startYear = gets.chomp.to_i
+puts 'Great! What is a ending year?'
+endYear = gets.chomp.to_i
+#leapYears = 
+puts 'Thanks! See all of the leap years between ' + startYear.to_s + ' and ' + endYear.to_s + ':'
+#puts leapYears
