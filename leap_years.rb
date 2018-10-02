579a88be72cf9bbdc277d919018e7375b4ae1830
Alena Panchenko <alena.panchenko22@gmail.com>
2018-10-02 12:22:13 +0300

    fix end leap year and optimize code

--- leap_years.rb
+++ leap_years.rb
@@ -1,28 +1,21 @@
 # leap_years excercise
-puts 1985%4
-puts 1984%4
 puts 'Hello! What is a starting year?'
 startYear = gets.chomp.to_i
 puts 'Great! What is a ending year?'
 endYear = gets.chomp.to_i
-residue4 = startYear%4 # leap
-residue100 = startYear%100 #not leap
-residue400 = startYear%400 #if 100 leap divisible by 400 - leap
-residueEnd = endYear%4
+puts 'There are the list of leap years between ' + startYear.to_s + ' end ' + endYear.to_s
 
-	while startYear < endYear # если последний год +4 выходит за рамки ендДейт - не выводить
-		if residue4 == 0
-		#puts startYear не могу вывести первый высокосный год
-		puts startYear = startYear + 4
-			else
-			startYear = startYear + 1
-			residue4 = startYear%4
-				if residue4 == 0
-				#puts startYear
-				puts startYear = startYear + 4
-				end
-		end
+while startYear <= endYear
+	residue4 = startYear%4 # leap
+	residue100 = startYear%100 #not leap
+	residue400 = startYear%400 #if 100 leap divisible by 400 - leap
+	if residue4 == 0 or (residue100 == 0 and residue400 == 0) # 1800 ділиться на 4 і на 100 - але це не високосний
+		puts startYear
+		startYear = startYear + 4
+	else
+		startYear = startYear + 1
 	end
+end
 
 
 
